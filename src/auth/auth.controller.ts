99f34c827ad20b384db458f1d3528a9c37f6c8c9
Damir Tagankhozhaev <tagankhozhaev@gmail.com>
2025-03-29 14:31:15 +0500

    3

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,12 +12,12 @@ export class AuthController {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
     });
     res.cookie('role', role, {
       httpOnly: false,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
     });
   }
 
